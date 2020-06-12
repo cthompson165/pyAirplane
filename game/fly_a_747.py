ecf32fc738c124fcd8dbd996eacfd8914bb8a70c
Thompson, Christian <CThompson@isolvedhcm.com>
2020-06-12 17:13:16 -0400

    clean up

--- game/fly_a_747.py
+++ game/fly_a_747.py
@@ -20,6 +20,7 @@ from aerodynamics.airplanes.seven_four_seven import SevenFourSeven
 from aerodynamics.simulator import Simulator
 from util.vector_2d import Vector2D
 from projector import Projector
+from sprites.explosion import Explosion
 
 
 class Colors:
@@ -151,44 +152,6 @@ class Plane(pygame.sprite.Sprite):
         control = -control  # it's backward
         throttle_control = (control + 1) / 2.0  # 0 to 1
         return 100 * throttle_control
-
-
-class Explosion(pygame.sprite.Sprite):
-    def __init__(self, center, size):
-
-        self._explosion_anim = {}
-        self._explosion_anim['lg'] = []
-        self._explosion_anim['sm'] = []
-        for i in range(9):
-            filename = 'images/regularExplosion0{}.png'.format(i)
-            img = pygame.image.load(filename).convert()
-            img.set_colorkey(Colors.BLACK)
-            img_lg = pygame.transform.scale(img, (75, 75))
-            self._explosion_anim['lg'].append(img_lg)
-            img_sm = pygame.transform.scale(img, (32, 32))
-            self._explosion_anim['sm'].append(img_sm)
-
-        pygame.sprite.Sprite.__init__(self)
-        self.size = size
-        self.image = self._explosion_anim[self.size][0]
-        self.rect = self.image.get_rect()
-        self.rect.center = center
-        self.frame = 0
-        self.last_update = pygame.time.get_ticks()
-        self.frame_rate = 50
-
-    def update(self):
-        now = pygame.time.get_ticks()
-        if now - self.last_update > self.frame_rate:
-            self.last_update = now
-            self.frame += 1
-            if self.frame == len(self._explosion_anim[self.size]):
-                self.kill()
-            else:
-                center = self.rect.center
-                self.image = self._explosion_anim[self.size][self.frame]
-                self.rect = self.image.get_rect()
-                self.rect.center = center
 
 
 class Cloud(pygame.sprite.Sprite):
